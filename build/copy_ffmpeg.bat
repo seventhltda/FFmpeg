4106c32ea67b9a4729207656af7f7e644bdb62e3
fernando-takeshi <fernando.takeshi@gmail.com>
2017-06-19 19:28:43 -0300

    Enables ffplay and local builds

--- build/copy_ffmpeg.bat
+++ build/copy_ffmpeg.bat
@@ -1,28 +1,39 @@
 setlocal
 set PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem
 
-net use "\\192.168.0.6\Releases\FFmpeg\3.2.2" seventh /user:jenkins
-rd /s /q \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-md \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
+if "%2%" == "" goto REMOTE
+if "%2%" == "local" goto LOCAL
+
+:REMOTE
+set destDir=\\192.168.0.6\Releases\FFmpeg\3.2.2
+net use "%destDir%" seventh /user:jenkins
+goto BUILD
+
+:LOCAL
+set destDir=D:\FFmpeg\build\3.2.2
+
+:BUILD
+rd /s /q %destDir%\%1%\
+md %destDir%\%1%\
 
 pushd D:\FFmpeg
-:: copy dependencies\*.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy libavcodec\avcodec-57.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy libavdevice\avdevice-57.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy libavfilter\avfilter-6.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy libavformat\avformat-57.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy libavutil\avutil-55.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy libpostproc\postproc-54.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy libswresample\swresample-2.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy libswscale\swscale-4.dll \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
-copy ffmpeg.exe \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
+copy libavcodec\avcodec-57.dll %destDir%\%1%\
+copy libavdevice\avdevice-57.dll %destDir%\%1%\
+copy libavfilter\avfilter-6.dll %destDir%\%1%\
+copy libavformat\avformat-57.dll %destDir%\%1%\
+copy libavutil\avutil-55.dll %destDir%\%1%\
+copy libpostproc\postproc-54.dll %destDir%\%1%\
+copy libswresample\swresample-2.dll %destDir%\%1%\
+copy libswscale\swscale-4.dll %destDir%\%1%\
+copy ffmpeg.exe %destDir%\%1%\
+copy ffplay.exe %destDir%\%1%\
 popd
 
 pushd D:\FFmpeg\build
-move /Y *.log \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
+move /Y *.log %destDir%\%1%\
 popd
 
-pushd \\192.168.0.6\Releases\FFmpeg\3.2.2\%1%\
+pushd %destDir%\%1%\
 ffmpeg.exe -formats > formats.txt
 ffmpeg.exe -codecs > codecs.txt
 ffmpeg.exe -protocols > protocols.txt
